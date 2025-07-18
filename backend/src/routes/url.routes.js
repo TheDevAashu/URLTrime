f10776f7723ed453a0936d3ea90658018c2a4699
Aashu Singh <aashusingh1308@gmail.com>
2025-07-18 18:28:18 +0530

    Remove backend to seperate repository as backend was not getting deployed

--- backend/src/routes/url.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { findLongUrl, shortenUrl } from "../controllers/url.controller.js";
-
-const urlRouter = express.Router();
-
-urlRouter.post("/shorten", shortenUrl);
-urlRouter.get("/:id", findLongUrl)
-
-export default urlRouter;
